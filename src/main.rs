fec8ce7d0f55945ab8efede4221227396288f1d7
Alexander Slesarev <alex.slesarev@gmail.com>
2025-02-11 22:35:27 -0500

    Fixed crashed and modernized the code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
@@ -21,14 +22,16 @@ pub struct RequestContext {
     pub stream: TcpStream,
     pub content_length: usize,
     pub buf: Vec<u8>,
+    verbose: bool,
 }
 
 impl RequestContext {
-    fn new(stream: TcpStream) -> Self {
+    fn new(stream: TcpStream, verbose: bool) -> Self {
         Self {
             stream,
-            buf: Vec::new(),
+            buf: Vec::with_capacity(32),
             content_length: 0,
+            verbose,
         }
     }
 
@@ -47,7 +50,9 @@ impl RequestContext {
         };
         self.buf.extend_from_slice(&buf);
         if self.buf.len() >= self.content_length {
-            println!("got all data: {} bytes", self.buf.len());
+            if self.verbose {
+                log(&format!("got all data: {} bytes", self.buf.len()));
+            }
             modify_interest(epoll_fd, self.stream.as_raw_fd(), listener_write_event(key))?;
         } else {
             modify_interest(epoll_fd, self.stream.as_raw_fd(), listener_read_event(key))?;
@@ -56,31 +61,41 @@ impl RequestContext {
     }
 
     fn parse_and_set_content_length(&mut self, data: &str) {
+        let content_length_slice = "content-length: ";
+        let content_length_sz = content_length_slice.len();
         if data.contains("HTTP") {
-            if let Some(content_length) = data
-                .lines()
-                .find(|l| l.to_lowercase().starts_with("content-length: "))
-            {
-                if let Some(len) = content_length
-                    .to_lowercase()
-                    .strip_prefix("content-length: ")
-                {
-                    self.content_length = len.parse::<usize>().expect("content-length is valid");
-                    println!("set content length: {} bytes", self.content_length);
+            if let Some(content_length) = data.lines().find(|l| {
+                l.len() > content_length_sz
+                    && l[..content_length_sz].eq_ignore_ascii_case(content_length_slice)
+            }) {
+                self.content_length = content_length[content_length_sz..]
+                    .parse::<usize>()
+                    .expect("content-length is valid");
+                if self.verbose {
+                    log(&format!(
+                        "set content length: {} bytes",
+                        self.content_length
+                    ));
                 }
             }
         }
     }
 
     fn write_cb(&mut self, key: u64, epoll_fd: RawFd) -> io::Result<()> {
-        match self.stream.write(HTTP_RESP) {
-            Ok(_) => println!("answered from request {}", key),
-            Err(e) => eprintln!("could not answer to request {}, {}", key, e),
+        match self.stream.write_all(HTTP_RESP) {
+            Ok(()) => {
+                if self.verbose {
+                    log(&format!("answered from request {key}"));
+                }
+            }
+            Err(e) => {
+                if self.verbose {
+                    log(&format!("could not answer to request {key}, {e}"));
+                }
+            }
         };
-        self.stream.shutdown(std::net::Shutdown::Both)?;
         let fd = self.stream.as_raw_fd();
         remove_interest(epoll_fd, fd)?;
-        close(fd);
         Ok(())
     }
 }
@@ -88,13 +103,30 @@ impl RequestContext {
 const READ_FLAGS: i32 = libc::EPOLLONESHOT | libc::EPOLLIN;
 const WRITE_FLAGS: i32 = libc::EPOLLONESHOT | libc::EPOLLOUT;
 
-const HTTP_RESP: &[u8] = br#"HTTP/1.1 200 OK
+const HTTP_RESP: &[u8] = br"HTTP/1.1 200 OK
 content-type: text/html
 content-length: 5
 
-Hello"#;
+Hello";
+
+#[cold]
+fn log(msg: &str) {
+    println!("{msg}");
+}
 
 fn main() -> io::Result<()> {
+    let mut verbose = false;
+
+    let args = env::args().skip(1);
+    for arg in args {
+        match &arg[..] {
+            "-v" | "--verbose" => {
+                verbose = true;
+            }
+            _ => {}
+        }
+    }
+
     let mut request_contexts: HashMap<u64, RequestContext> = HashMap::new();
     let mut events: Vec<libc::epoll_event> = Vec::with_capacity(1024);
     let mut key = 100;
@@ -107,20 +139,24 @@ fn main() -> io::Result<()> {
     add_interest(epoll_fd, listener_fd, listener_read_event(key))?;
 
     loop {
-        println!("requests in flight: {}", request_contexts.len());
+        if verbose {
+            log(&format!("requests in flight: {}", request_contexts.len()));
+        }
         events.clear();
         let res = match syscall!(epoll_wait(
             epoll_fd,
-            events.as_mut_ptr() as *mut libc::epoll_event,
+            events.as_mut_ptr(),
             1024,
             1000 as libc::c_int,
         )) {
             Ok(v) => v,
-            Err(e) => panic!("error during epoll wait: {}", e),
+            Err(e) => panic!("error during epoll wait: {e}"),
         };
 
-        // safe  as long as the kernel does nothing wrong - copied from mio
-        unsafe { events.set_len(res as usize) };
+        #[allow(clippy::cast_sign_loss)]
+        unsafe {
+            events.set_len(res as usize);
+        };
 
         for ev in &events {
             match ev.u64 {
@@ -128,28 +164,39 @@ fn main() -> io::Result<()> {
                     match listener.accept() {
                         Ok((stream, addr)) => {
                             stream.set_nonblocking(true)?;
-                            println!("new client: {}", addr);
+                            if verbose {
+                                log(&format!("new client: {addr}"));
+                            }
                             key += 1;
                             add_interest(epoll_fd, stream.as_raw_fd(), listener_read_event(key))?;
-                            request_contexts.insert(key, RequestContext::new(stream));
+                            request_contexts.insert(key, RequestContext::new(stream, verbose));
                         }
-                        Err(e) => eprintln!("couldn't accept: {}", e),
+                        Err(e) => {
+                            if verbose {
+                                log(&format!("couldn't accept: {e}"));
+                            }
+                        }
                     };
                     modify_interest(epoll_fd, listener_fd, listener_read_event(100))?;
                 }
                 key => {
                     let mut to_delete = None;
                     if let Some(context) = request_contexts.get_mut(&key) {
-                        let events: u32 = ev.events;
+                        #[allow(clippy::cast_possible_wrap)]
+                        let events = ev.events as i32;
                         match events {
-                            v if v as i32 & libc::EPOLLIN == libc::EPOLLIN => {
+                            v if v & libc::EPOLLIN == libc::EPOLLIN => {
                                 context.read_cb(key, epoll_fd)?;
                             }
-                            v if v as i32 & libc::EPOLLOUT == libc::EPOLLOUT => {
+                            v if v & libc::EPOLLOUT == libc::EPOLLOUT => {
                                 context.write_cb(key, epoll_fd)?;
                                 to_delete = Some(key);
                             }
-                            v => println!("unexpected events: {}", v),
+                            v => {
+                                if verbose {
+                                    log(&format!("unexpected events: {v}"));
+                                }
+                            }
                         };
                     }
                     if let Some(key) = to_delete {
@@ -182,10 +229,6 @@ fn listener_write_event(key: u64) -> libc::epoll_event {
         events: WRITE_FLAGS as u32,
         u64: key,
     }
-}
-
-fn close(fd: RawFd) {
-    let _ = syscall!(close(fd));
 }
 
 fn add_interest(epoll_fd: RawFd, fd: RawFd, mut event: libc::epoll_event) -> io::Result<()> {
